20789861ed578af9f27e05eff51772ee589e0394
Darmin-Geek <jackmhankin@gmail.com>
2026-08-03 13:00:28 -0400

    Rename to SIAM and remove old docs

--- docuscope/app/layout.tsx
+++ docuscope/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Docuscope",
+  title: "SIAM (Source and Information Analysis Manager)",
   description: "Description TBD",
 };
 
